6d1bd4a2b75eb30b4b161b1aaed0250a51eaa7c1
fatemah-alm <fatemah.alm@hotmail.com>
2022-01-18 13:16:29 +0400

    done yay

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flowers shop</h1>
+      <p>The best flower shop in town!❤️</p>
+      <img
+        src="https://cdn3.volusion.com/ywjzv.xdxbb/v/vspfiles/photos/80266-2.jpg?v-cache=1626951301"
+        alt="flower shop"
+      />
+    </div>
+  );
+}
+
+export default App;
